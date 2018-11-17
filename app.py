68f1f82de8bc55350b804d13cba374bd8462fe51
gafmn <madina.gafarova@yandex.ru>
2018-11-17 10:46:58 +0300

    ER

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ sql_create_charging_station_table = """CREATE TABLE IF NOT EXISTS charging_stati
                                             amount_of_available_slots integer NOT NULL,
                                             time_of_charging time NOT NULL, 
                                             price double
+                                            GPS_location varchar(25) NOT NULL 
                             );"""
 
 sql_create_charging_plugs = """CREATE TABLE IF NOT EXISTS charging_plugs(
@@ -44,7 +45,7 @@ sql_create_provider_table = """CREATE TABLE IF NOT EXISTS provider(
                                     company_id integer PRIMARY KEY,
                                     address varchar(25) NOT NULL,
                                     phone_number varchar(25),
-                                    name varchar(25)
+                                    name_company varchar(25)
 
                         );"""
 
@@ -100,6 +101,15 @@ sql_create_workshop_table = """CREATE TABLE IF NOT EXISTS workshop(
 
                         );"""
 
+sql_create_repair_car = """CREATE TABLE IF NOT EXISTS repair_car(
+                                WID integer,
+                                car_id integer, 
+                                report_id integer PRIMARY KEY,
+                                date date,
+                                prograss_status varchar(10),
+                                FOREIGN KEY (WID) references workshop(WID),
+                                FOREIGN KEY (car_id) references cars(car_id)
+                    );"""
 
 
 sql_create_models = """CREATE TABLE IF NOT EXISTS models(
@@ -112,10 +122,21 @@ sql_create_models = """CREATE TABLE IF NOT EXISTS models(
                         foreign key (model_id) references charging_plugs(plug_id)
                     );"""
 
+sql_create_part_order_table = """CREATE TABLE IF NOT EXISTS part_order(
+                                    date date,
+                                    amount integer,
+                                    cost double,
+                                    order_id integer PRIMARY KEY 
+                        );"""
+
 sql_create_parts_table = """CREATE TABLE IF NOT EXISTS parts(
                                 part_id integer PRIMARY KEY, 
                                 type_of_detail varchar(25) NOT NULL
 
+                        );"""
+
+sql_create_have_parts_table = """CREATE TABLE IF NOT EXISTS have_parts(
+                                    
                         );"""
 
 sql_create_fit_table = """CREATE TABLE IF NOT EXISTS fit(
@@ -124,6 +145,8 @@ sql_create_fit_table = """CREATE TABLE IF NOT EXISTS fit(
                                 FOREIGN KEY (part_id) references parts(part_id),
                                 FOREIGN KEY (model_id) references models(model_id)
                 );"""
+
+
 
 
 def create_connection(db_file):
