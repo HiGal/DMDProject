7ea8c1828dded538cd25901eef4dd36e72b372d4
HiGal <f.galeev@innopolis.ru>
2018-11-17 10:47:13 +0300

    add unique to entety's primary keys

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
 import sqlite3
-import os
 
 app = Flask(__name__)
 
 db_file = 'carsharing.sqlite'
 
 sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks(
-                                id integer PRIMARY KEY,
+                                id integer UNIQUE PRIMARY KEY,
                                 name text NOT NULL,
                                 priority integer,
                                 status_id integer NOT NULL,
@@ -18,27 +17,27 @@ sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks(
                             );"""
 
 sql_create_charging_station_table = """CREATE TABLE IF NOT EXISTS charging_station(
-                                  UID integer PRIMARY KEY,
+                                  UID integer UNIQUE PRIMARY KEY,
                                   amount_of_available_slots integer NOT NULL,
                                   time_of_charging time NOT NULL, 
                                   price double
                             );"""
 
 sql_create_parts_table = """CREATE TABLE IF NOT EXISTS parts(
-                                part_id integer PRIMARY KEY, 
+                                part_id integer UNIQUE PRIMARY KEY, 
                                 type_of_detail varchar(25) NOT NULL
 
                         );"""
 
 sql_create_workshop_table = """CREATE TABLE IF NOT EXISTS workshop(
-                                    WID integer PRIMARY KEY ,
+                                    WID integer UNIQUE PRIMARY KEY ,
                                     availability_of_timing time NOT NULL,
                                     location varchar(25) NOT NULL
 
                         );"""
 
 sql_create_provider_table = """CREATE TABLE IF NOT EXISTS provider(
-                                    company_id integer PRIMARY KEY,
+                                    company_id integer UNIQUE PRIMARY KEY,
                                     address varchar(25) NOT NULL,
                                     phone_number varchar(25),
                                     name varchar(25)
@@ -46,7 +45,7 @@ sql_create_provider_table = """CREATE TABLE IF NOT EXISTS provider(
                         );"""
 
 sql_create_customers_table = """CREATE TABLE IF NOT EXISTS customers (
-                                  username  varchar(20) PRIMARY KEY ,
+                                  username  varchar(20) UNIQUE PRIMARY KEY ,
                                   email  varchar(20) not null ,
                                   cardnumber    varchar(20) not null,
                                   fullname   varchar(50) not null,
@@ -61,7 +60,7 @@ sql_create_customers_table = """CREATE TABLE IF NOT EXISTS customers (
 # cost and duration?
 # st_point, pick location same?
 sql_create_orders = """CREATE TABLE IF NOT EXISTS orders (
-                        order_id integer PRIMARY KEY,
+                        order_id integer UNIQUE PRIMARY KEY,
                         date text not null ,
                         time text not null ,
                         date_closed text not null,
@@ -70,7 +69,6 @@ sql_create_orders = """CREATE TABLE IF NOT EXISTS orders (
                         cost integer,
                         st_point varchar(50) not null ,
                         destination varchar(50) not null ,
-                        pick_location varchar(50) not null ,
                         car_location varchar(50) not null, 
                         
                         foreign key (order_id) references customers(username)
@@ -78,29 +76,28 @@ sql_create_orders = """CREATE TABLE IF NOT EXISTS orders (
                     );"""
 
 sql_create_cars = """CREATE TABLE IF NOT EXISTS cars(
-                        car_id integer primary key ,
+                        car_id integer unique primary key ,
                         gps_location varchar(25) not null ,
                         year varchar(4),
                         reg_num varchar(11) not null ,
                         charge int(1) not null ,
                         available int(1) not null ,
                         
-                        foreign key (car_id) references orders(order_id)
-    
+                        foreign key (car_id) references orders(order_id),
+                        foreign key (car_id) references models(model_id)
                     );"""
 
 sql_create_models = """CREATE TABLE IF NOT EXISTS models(
-                        model_id integer PRIMARY KEY ,
+                        model_id integer unique PRIMARY KEY ,
                         name varchar(20) not null,
                         type varchar(30) not null ,
                         service_class varchar(30) not null ,
                         
-                        foreign key (model_id) references cars(car_id),
                         foreign key (model_id) references charging_plugs(plug_id)
                     );"""
 
 sql_create_charging_plugs = """CREATE TABLE IF NOT EXISTS charging_plugs(
-                                plug_id integer PRIMARY KEY ,
+                                plug_id integer unique PRIMARY KEY ,
                                 shape_plug varchar(20) not null  ,
                                 size_plug int(10) not null 
                             );"""
@@ -150,6 +147,6 @@ def hello_world():
 if __name__ == '__main__':
     conn = create_connection(db_file)
     print(conn)
-    create_table(conn,'sql_create_tasks_table')
+    create_table(conn, 'sql_create_tasks_table')
     close_connection(conn)
     app.run()
