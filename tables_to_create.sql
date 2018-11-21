d934b799a40ee65c90084ddc6aaf78ac35e9b784
gafmn <madina.gafarova@yandex.ru>
2018-11-21 17:18:14 +0300

    Edition of tables

--- tables_to_create.sql
+++ tables_to_create.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS tasks (
 );
 
 CREATE TABLE IF NOT EXISTS charging_station (
-  UID                       integer UNIQUE PRIMARY KEY,
-  amount_of_available_slots integer                  NOT NULL,
-  time_of_charging          time                     NOT NULL,
-  price                     double
-                            GPS_location varchar(25) NOT NULL
+  UID                       integer PRIMARY KEY,
+  amount_of_available_slots integer     NOT NULL,
+  time_of_charging          time        NOT NULL,
+  price                     double,
+  GPS_location              varchar(25) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS charging_plugs (
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS charging_plugs (
   size_plug  int(10)     not null
 );
 
-CREATE TABLE IF NOT EXISTS charging_have_plugs (
+CREATE TABLE IF NOT EXISTS stations_have_plugs (
   charge_have_plugs_id integer NOT NULL,
   UID                  integer NOT NULL,
   plug_id              integer NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS provider (
 
 );
 CREATE TABLE IF NOT EXISTS customers (
-  username     varchar(20) UNIQUE PRIMARY KEY,
+  username     varchar(20) PRIMARY KEY,
   email        varchar(20) not null,
   cardnumber   varchar(20) not null,
   fullname     varchar(50) not null,
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS customers (
 TODO st_point, pick location same?*/
 
 CREATE TABLE IF NOT EXISTS orders (
-  order_id     integer UNIQUE PRIMARY KEY,
+  order_id     integer PRIMARY KEY,
   date         text        not null,
   time         text        not null,
   date_closed  text        not null,
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS cars (
   foreign key (car_id) references models (model_id)
 );
 
-CREATE TABLE IF NOT EXISTS charge_car (
+CREATE TABLE IF NOT EXISTS charge_car_history (
   charge_car_id integer PRIMARY KEY,
   cost          double,
   date          date,
