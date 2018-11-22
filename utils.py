fb982dc4c0f69f2b304adbf456caa99ec16f9454
Alexeyzhu <alekseyzhuchkov.tut@gmail.com>
2018-11-22 10:06:39 +0300

    Fill the db

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import ssl
 from geopy.geocoders import Nominatim
 import geopy.geocoders
 import datetime
+
 ctx = ssl.create_default_context(cafile=certifi.where())
 geopy.geocoders.options.default_ssl_context = ctx
 
@@ -12,12 +13,17 @@ fake = Faker()
 
 
 def fill_db_with_data(conn):
+    geolocator = Nominatim(user_agent="dmd_project")
     users = []
     plugs = []
     cars = []
-    type_car = {"Hatchback", "Sedan", "Crossover", "Coupe", "Convertible"}
-    service_class_car = {"comfort", "economy", "business "}
-    name_car = {"Chevy Sonic", "Ford Fiesta", "Honda Fit", "Mitsubishi Mirage", "Kia Rio"}
+    models =[]
+    colors = ["red", "yellow", "green", "blue", "black", "white"]
+    reg_name = ["AN", "ER", "TC", "NZ", "FG", "AZ", "MG"]
+    type_car = ["Hatchback", "Sedan", "Crossover", "Coupe", "Convertible"]
+    service_class_car = ["comfort", "economy", "business "]
+    name_car = ["Chevy Sonic", "Ford Fiesta", "Honda Fit", "Mitsubishi Mirage", "Kia Rio"]
+
     # create parameters for plugs
     for i in range(5):
         shape_of_plugs = random.randint(100, 999)
@@ -26,11 +32,22 @@ def fill_db_with_data(conn):
         plugs.append(task)
         print(task)
         # insert_into_plugs(conn, task)
+    #
     for i in range(5):
-        type = type_car[random.randint(0,len(type_car) - 1)]
+        type = type_car[random.randint(0, len(type_car) - 1)]
         service_of_class = service_class_car[random.randint(0, len(service_class_car) - 1)]
         name = name_car[random.randint(0, len(name_car) - 1)]
-        task = (plugs[i // len(plugs)], )
+        task = (plugs[i // len(plugs)],)
+
+    for i in range(10):
+        location = geolocator.reverse(random.uniform(40.1, 41.1), random.uniform(-74.4, -73.8))
+        gps_location = str(location.latitude) + " " + str(location.longitude)
+        year = random.randint(1985,2012)
+        regnum = reg_name[i//len(reg_name)] + str(random.randint(1000, 9999))
+        task = (gps_location,year,colors[i//len(colors)],regnum,
+                random.randint(10,99),"available",model_id)
+
+    # fill customer table
     for i in range(5):
         address = str(fake.address()).replace('\n', '')
         name = fake.name()
@@ -48,24 +65,7 @@ def fill_db_with_data(conn):
         print(task)
         # insert_into_customers(conn, task)
 
-    for i in range(5):
-        address = str(fake.address()).replace('\n', '')
-        name = fake.name()
-        username = str(name).replace(' ', '').lower()
-        email = username + "@gmail.com"
-        task = (username,
-                email,
-                random.randint(1000000000000000, 9999999999999999),
-                name,
-                random.randint(10000000000, 99999999999),
-                random.randint(100000, 999999),
-                address
-                )
-        users.append(task)
-        print(task)
-        # insert_into_customers(conn, task)
-
-    geolocator = Nominatim(user_agent="dmd_project")
+    # fill order table
     for i in range(10):
         date = datetime.date.today()
         status = "closed"
